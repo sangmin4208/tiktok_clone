4f78313a3d3d7d993ee476a2089b1bd6705ba5f1
sangmin <sangmin4208@gmail.com>
2023-02-01 16:59:40 +0900

    😇 #6.3 NavigationBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,68 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  final screens = [
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Search'),
+    ),
+    const Center(
+      child: Text('3'),
+    ),
+    const Center(
+      child: Text('4'),
+    ),
+    const Center(
+      child: Text('5'),
+    ),
+  ];
+
+  int _selectedIndex = 0;
+
+  _onDestinationSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onDestinationSelected,
+        destinations: const [
+          NavigationDestination(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: 'Search',
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/const/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -14,7 +14,7 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFE9435A),
+        primarySwatch: const Color(0xFFE9435A).toMaterialColor(),
         appBarTheme: const AppBarTheme(
           elevation: 0,
           backgroundColor: Colors.white,
@@ -25,8 +25,35 @@ class TikTokApp extends StatelessWidget {
             color: Colors.black,
           ),
         ),
+        navigationBarTheme: const NavigationBarThemeData(
+          backgroundColor: Colors.white,
+          indicatorColor: Colors.pink,
+          iconTheme: MaterialStatePropertyAll(
+            IconThemeData(
+              color: Colors.grey,
+              size: Sizes.size24,
+            ),
+          ),
+        ),
       ),
-      home: const SignUpScreen(),
+      home: const MainNavigationScreen(),
     );
+  }
+}
+
+extension ToMaterialColor on Color {
+  MaterialColor toMaterialColor() {
+    return MaterialColor(value, {
+      50: withOpacity(0.1),
+      100: withOpacity(0.2),
+      200: withOpacity(0.3),
+      300: withOpacity(0.4),
+      400: withOpacity(0.5),
+      500: withOpacity(0.6),
+      600: withOpacity(0.7),
+      700: withOpacity(0.8),
+      800: withOpacity(0.9),
+      900: withOpacity(1),
+    });
   }
 }
