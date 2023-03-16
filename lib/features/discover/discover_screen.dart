1a5611d3f7de6f4bd8f8e2b864982455c6a3785e
sangmin <sangmin4208@gmail.com>
2023-03-16 23:06:04 +0900

    #9.4 Grid Item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/const/gaps.dart';
 import 'package:tiktok_clone/const/sizes.dart';
 
 final tabs = [
@@ -26,7 +28,13 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
       child: Scaffold(
           appBar: AppBar(
             elevation: 1,
-            title: const Text('Discover'),
+            title: const Text(
+              "Discover",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size20,
+              ),
+            ),
             bottom: TabBar(
               isScrollable: true,
               indicatorColor: Colors.black,
@@ -56,12 +64,77 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                 crossAxisCount: 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 21,
               ),
               itemCount: tabs.length,
               itemBuilder: (context, index) {
-                return Container(
-                  color: Colors.grey.shade300,
+                return Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Sizes.size4),
+                      ),
+                      clipBehavior: Clip.antiAlias,
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage(
+                          placeholder:
+                              const AssetImage('assets/images/placeholder.png'),
+                          image: NetworkImage(
+                              "https://picsum.photos/seed/$index/200/200"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Gaps.v10,
+                    const Text(
+                      "lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size16 + Sizes.size2,
+                      ),
+                    ),
+                    Gaps.v5,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: Sizes.size14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: Sizes.size14,
+                            backgroundImage: NetworkImage(
+                                "https://picsum.photos/seed/$index/200/200"),
+                          ),
+                          Gaps.h5,
+                          const Expanded(
+                            child: Text(
+                              "lorem ipsum dolor sit amet",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: Sizes.size16,
+                              ),
+                            ),
+                          ),
+                          const FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size14,
+                            color: Colors.grey,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "1.2M",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 );
               },
             ),
